79ef2d5335b530b09f54a7a130218c84dbc7f9d2
bill <bill@bill-M750SLI-DS4>
2012-07-02 14:35:16 -0700

    changed the way the network connects

--- src/simulations/polychrony.py
+++ src/simulations/polychrony.py
@@ -4,6 +4,11 @@ Izhikevich - 2006 - Polychronization: Computation with Spikes
 
 author: bill lennon
 date: 18 June 2012
+
+TO DO:
+    - Get STDP working properly.  Nearest spike learning only.
+    - Monitor synaptic weight changes
+    - Find/track polychronous groups
 '''
 from brian.library.IF import *
 from brian.neurongroup import *
@@ -14,98 +19,119 @@ from utils import *
 import time
 import matplotlib.pyplot as plt
 
-defaultclock.dt = .5*ms
+set_global_preferences(useweave=True)
 print 'building network...'
 start = time.time()
 
 ########################################################################
 # Network Parameters
 ########################################################################
+sim_clock = Clock(dt=0.5*ms)    # simulation clock
+input_clock = Clock(dt=1*ms)    # input current change clock
+mon_clock = Clock(dt=1*ms)      # monitor clock
+report_clock = Clock(dt=1000*ms)
 Ne, Ni = 800, 200   # number of excitatory/inhibitory neurons
 N = Ne + Ni
-sm = 10 * mV        # max synapse strength
+smax = 10 * mV        # max synapse strength
+tau_pre = 20*ms
+tau_post = 20*ms
+dA_pre = smax*.005
+dA_post = -dA_pre*1.05
+plot_on = True
 
 
 ########################################################################
 # Neurons
-########################################################################        
-Ge = IzhikevichGroup(Ne, 0.02/ms, 0.2/ms, -65*mV, 8*mV/ms, 1*ms, 1*ms)
-Gi = IzhikevichGroup(Ni, 0.1/ms, 0.2/ms, -65*mV, 2*mV/ms, 1*ms, 1*ms)
+########################################################################
+G = IzhikevichGroup(N, 'a', 0.2/ms, -65*mV, 'd', 1*ms, 1*ms, clock=sim_clock)        
+Ge = G.subgroup(Ne)
+Gi = G.subgroup(Ni)
+Ge.a,Ge.d = 0.02/ms, 8*mV/ms
+Gi.a,Gi.d = 0.1/ms, 2*mV/ms
 
 
 ########################################################################    
 # Connections
 ########################################################################
-# Ge -> Ge
-w,d = random_weights_delays(Ne,Ne,.1,max_weight=2.0*mV,max_delay=20*ms)
-Cee = DelayConnection(Ge,Ge,'ge', delay=d)
-Cee.connect(Ge,Ge,w)
+# Ge -> G
+Ce = DelayConnection(Ge,G,max_delay=20*ms)
+Ce.connect_random(Ge,G,.05,weight=6.0*mV,delay=(0*ms,20*ms))
 
-# Ge -> Gi
-w,d = random_weights_delays(Ne,Ni,.1,max_weight=2.0*mV,max_delay=20*ms)
-Cei = DelayConnection(Ge,Gi,'ge', delay=d)
-Cei.connect(Ge,Gi,w)
+Ci = DelayConnection(Gi,Ge,max_delay=1*ms)
+Ci.connect_random(Gi,Ge,.05,weight=-5.0*mV,delay=1*ms)
+'''
+w,d = random_weights_delays(Ne,N,.05,max_weight=6.0*mV,max_delay=20*ms)
+Ce = DelayConnection(Ge,G,'ge', delay=d)
+Ce.connect(Ge,G,w)
 
 # Gi -> Ge
-w,d = random_weights_delays(Ni,Ne,.1,max_weight=-5.0*mV,max_delay=1*ms)
-Cie = DelayConnection(Gi,Ge,'gi', delay=d)
-Cie.connect(Gi,Ge,w)
+w,d = random_weights_delays(Ni,Ne,.05,max_weight=-5.0*mV,max_delay=1*ms)
+Ci = DelayConnection(Gi,Ge,'gi', delay=d)
+Ci.connect(Gi,Ge,w)
+'''
+stdp = ExponentialSTDP(Ce, tau_pre, tau_post, dA_pre, dA_post, wmax=smax,
+                       clock=sim_clock)
 
 
 ########################################################################
 # Input
 ########################################################################
-@network_operation
+@network_operation(input_clock)
 def thalamic_input():
-    Ge.I = 6*np.random.rand(Ne)*nA
-    Gi.I = 2*np.random.rand(Ni)*nA
+    stim = np.zeros_like(G.I)
+    stim[np.random.randint(stim.shape[0])] = 20 * nA
+    G.I = stim
 
 
 ########################################################################
 # Monitor
 ########################################################################
 Mse = SpikeMonitor(Ge)
-Mve = StateMonitor(Ge, 'v', record=[0,Ne-1])
-MIe = StateMonitor(Ge, 'I', record=[0,Ne-1])
+Mve = StateMonitor(Ge, 'v', record=[0,Ne-1], clock=mon_clock)
+MIe = StateMonitor(Ge, 'I', record=[0,Ne-1], clock=mon_clock)
 Msi = SpikeMonitor(Gi)
-Mvi = StateMonitor(Gi, 'v', record=[0,Ni-1])
-MIi = StateMonitor(Gi, 'I', record=[0,Ni-1])
+Mvi = StateMonitor(Gi, 'v', record=[0,Ni-1], clock=mon_clock)
+MIi = StateMonitor(Gi, 'I', record=[0,Ni-1], clock=mon_clock)
 M = [Mse,Mve,MIe,Msi,Mvi,MIi]
 
 
 ########################################################################
 # Plotting
 ########################################################################
-ion()
-figure(0,figsize=(20,10))
-plot_args = dict(refresh=500*ms, showlast=500*ms)
-subplot(221)
-raster_plot(Mse, title='Exc Neurons - Firing Times', newfigure=False, **plot_args)
-xlabel('time (ms)')
-subplot(222)
-Mve.plot(**plot_args)
-title('Excitatory Neurons trace')
-xlabel('time (s)')
-ylabel('membrane potential (V)')
-subplot(223)
-raster_plot(Msi, title='Inh Neurons - Firing Times', newfigure=False, **plot_args)
-xlabel('time (ms)')
-subplot(224)
-Mvi.plot(**plot_args)
-title('Inhibitory Neurons trace')
-xlabel('time (s)')
-ylabel('membrane potential (V)')
-subplots_adjust(hspace=0.25)
+if plot_on:
+    ion()
+    figure(0,figsize=(18,10))
+    plot_args = dict(refresh=1000*ms, showlast=1000*ms)
+    subplot(221)
+    raster_plot(Mse, title='Exc Neurons - Firing Times', newfigure=False, **plot_args)
+    xlabel('time (ms)')
+    subplot(222)
+    Mve.plot(**plot_args)
+    title('Excitatory Neurons trace')
+    xlabel('time (s)')
+    ylabel('membrane potential (V)')
+    subplot(223)
+    raster_plot(Msi, title='Inh Neurons - Firing Times', newfigure=False, **plot_args)
+    xlabel('time (ms)')
+    subplot(224)
+    Mvi.plot(**plot_args)
+    title('Inhibitory Neurons trace')
+    xlabel('time (s)')
+    ylabel('membrane potential (V)')
+    subplots_adjust(hspace=0.25)
 
+@network_operation(report_clock)
+def report_weights():
+    print 'Total excitatory synaptic value: ', np.sum(Ce.W.alldata)
 
 ########################################################################
 # Run simulation
 ########################################################################
-net = Network(Ge,Gi,Cee,Cei,Cie,M,thalamic_input)
+net = Network(G,M,Ce,Ci,stdp,thalamic_input,report_weights)
 print 'network built. took %s seconds' % (time.time()-start)
 print 'running...'
 start = time.time()
-net.run(3000 * ms)
+net.run(20000*ms, report='text')
 print 'done. took %s seconds' % (time.time()-start)
 ioff()
 show()
